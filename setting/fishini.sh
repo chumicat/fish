a508587291bb21a24bae3597d7fce5cdc6bc8c99
chumicat <russell.tseng@104.com.tw>
2020-04-08 09:37:07 +0800

    fishini

---
+++ setting/fishini.sh
@@ -0,0 +1,5 @@
+# run this shell in your computer to set it correctly
+mv ~/.config/fish/config.fish ~
+rm -rf ~/.config/fish
+git clone git@github.com:chumicat/fish.git ~/.config
+mv ~/config.fish ~/.config/fish/config.fish
