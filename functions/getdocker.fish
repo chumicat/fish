851193fd52ce9636c87ff152e4f82dbd99c12d7f
chumicat <russell.tseng@104.com.tw>
2020-04-10 11:49:34 +0800

    autoupdate

---
+++ functions/getdocker.fish
@@ -0,0 +1,5 @@
+function getdocker
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm get-docker.sh
+end
--- functions/getomf.fish
+++ functions/getomf.fish
@@ -1,3 +1,3 @@
-function installomf
+function getomf
   curl -L https://get.oh-my.fish | fish
 end
