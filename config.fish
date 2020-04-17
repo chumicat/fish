32dfc4bd8dea9a0535bdad86fe152f582e113b70
chumicat <russell.tseng@104.com.tw>
2020-04-17 16:43:14 +0800

    autoupdate

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
 set PATH $PATH /home/linuxbrew/.linuxbrew/bin
 set -g fish_user_paths "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths
 set PATH $PATH $HOME/.composer/vendor/bin
-export PATH="$HOME/.composer/vendor/bin:$PATH"
+set PATH $PATH $HOME/opt
