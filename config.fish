f2ddc324f3bcd1fbc4fb382b3ab3d563428c0079
chumicat <russell.tseng@104.com.tw>
2020-04-17 20:52:29 +0800

    autoupdate

--- config.fish
+++ config.fish
@@ -2,4 +2,3 @@ set PATH $PATH /home/linuxbrew/.linuxbrew/bin
 set -g fish_user_paths "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths
 set PATH $PATH $HOME/.composer/vendor/bin
 set -g fish_user_paths "/home/linuxbrew/.linuxbrew/opt/python@3.8/bin" $fish_user_paths
-set PATH $PATH $HOME/opt
