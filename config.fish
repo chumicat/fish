255af4f3ad57e2874d11d4b1ea30e1732fb38f9f
chumicat <russell.tseng@104.com.tw>
2020-04-27 17:12:56 +0800

    autoupdate

--- config.fish
+++ config.fish
@@ -2,3 +2,4 @@ set PATH $PATH /home/linuxbrew/.linuxbrew/bin
 set -g fish_user_paths "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths
 set PATH $PATH $HOME/.composer/vendor/bin
 set -g fish_user_paths "/home/linuxbrew/.linuxbrew/opt/python@3.8/bin" $fish_user_paths
+set PATH $PATH /usr/local/Homebrew/Library/Taps/chumicat/homebrew-lara
